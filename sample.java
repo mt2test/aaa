6fe17c4545c601987be06afd9cb5b58a9efc7747
hiromaru3 <toranekousagi0331@yahoo.co.jp>
2014-10-03 14:48:52 +0900

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,11 @@
+// Javaのサンプルプログラムです。         aaaaiiiiii
+ 
+public class Hellow {
+	public  static void main(String args[]){
+		//この下から、内容が順次実行されます
+		System.out.println("Hellow Java");//Hellow Javaと出力
+		System.out.println("こんにちは");//こんにちはと出力
+		System.out.println("ニーハオ");//ニーハオと出力
+		System.out.println("追加テスト");//追加テストと出力
+	}
+}
