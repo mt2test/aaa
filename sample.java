0f249cb3e32a5a714bd0a9b08ffa96aa034adbe4
hawkkk <yuichi19930508@gmail.com>
2014-10-03 14:59:51 +0900

    Update sample.java

--- sample.java
+++ sample.java
@@ -5,7 +5,7 @@ public class Hellow {
 		//この下から、内容が順次実行されます
 		System.out.println("Hellow Java");//Hellow Javaと出力
 		System.out.println("こんにちは");//こんにちはと出力
-		System.out.println("ニーハオ");//ニーハオと出力
+		System.out.println("ありがとナス！");//ありがとナス！と出力
 		System.out.println("追加テスト");//追加テストと出力
 		System.out.println("pull request");//追加テスト２
 	}
